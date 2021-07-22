0870eadb8d61ddb7fc6cf11d82a2f2aa64043977
kvanst <kvanst3@gmail.com>
2021-07-22 21:43:13 +0200

    listen on space hit

--- main.py
+++ main.py
@@ -15,22 +15,24 @@ class TypeSpeed():
         self.listbox = tk.Listbox()
         self.listbox.pack(expand=True, fill='both')
 
-        string_listener = tk.StringVar()
-        string_listener.set("Init Text")
-        string_listener.trace("w", self.text_changed)
+        self.string_listener = tk.StringVar()
+        self.string_listener.set("Init Text")
 
-        self.entry_widget = tk.Entry(self.window, textvariable=string_listener)
+        self.entry_widget = tk.Entry(self.window, textvariable=self.string_listener)
         self.entry_widget.pack(expand=True, fill='x')
 
         self.count = 60
         self.count_switch = False
+
+        self.window.bind("<space>", self.text_changed)
 
         self.window.mainloop()
 
 
 
     def text_changed(self, *args):
-        print("Text changed.")
+        self.string_listener.set(" ")
+        print("here")
         if self.count_switch is False:
             self.count_down()
             self.count_switch = True
