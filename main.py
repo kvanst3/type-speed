2491fcb4356bab1cb812100d244a6fe060b44dd5
kvanst <kvanst3@gmail.com>
2021-07-22 14:34:22 +0200

    general layout completed

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-import tkinter
-from typing import Type
+import tkinter as tk
+from tkinter.constants import E, LEFT
 
 class TypeSpeed():
 
     def __init__(self):
-        window = tkinter.Tk()
+        window = tk.Tk()
         window.title("Speed Typer")
-        window.minsize(width=800, height=800)
+        window.minsize(width=700, height=700)
         window.config(padx=10, pady=10)
+
+        self.time_label = tk.Label(text="Time Left")
+        self.time_label.pack(expand=True)
+
+        self.listbox = tk.Listbox()
+        self.listbox.pack(expand=True, fill='both')
+
+        string_listener = tk.StringVar()
+        string_listener.set("Init Text")
+        string_listener.trace("w", self.text_changed)
+
+        self.entry_widget = tk.Entry(window, textvariable=string_listener)
+        self.entry_widget.pack(expand=True, fill='x')
 
 
         window.mainloop()
+
+
+
+    def text_changed(self, *args):
+        print("Text changed.")
 
 
 
